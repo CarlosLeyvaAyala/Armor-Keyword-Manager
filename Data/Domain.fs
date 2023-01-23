2d509077e3c7e0a7c3b8d96c28950ce9d073bc1d
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-01-22 18:44:37 -0600

    First commit

---
+++ Data/Domain.fs
@@ -0,0 +1,5 @@
+﻿namespace Data
+
+type EDID = string
+type Keyword = string
+type ArmorMap = Map<EDID, Keyword list>
---
+++ Data/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Data
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name

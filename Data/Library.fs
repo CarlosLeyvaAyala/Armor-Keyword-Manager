3ff91b1b19b1f2abd199dc1778ae9911813503ec
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-01-23 21:10:51 -0600

    Change folder structure

--- Data/Library.fs
+++ Data/Library.fs
@@ -1,5 +1,10 @@
 ﻿namespace Data
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module private Collections =
+    let ListToCList (a: List<'a>) =
+        let l = System.Collections.Generic.List<'a>()
+
+        for i in 0 .. a.Length - 1 do
+            l.Add(a[i])
+
+        l

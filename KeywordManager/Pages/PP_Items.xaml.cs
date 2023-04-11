7d3a70e325b62cc5a16f273c533ec7b7517aab50
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-04-11 14:22:16 -0600

    Move tag filter to drawer

--- KeywordManager/Pages/PP_Items.xaml.cs
+++ KeywordManager/Pages/PP_Items.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -37,9 +38,9 @@ public partial class PP_Items : UserControl {
   private void LoadKeywords(IEnumerable? list) => lstKeywords.ItemsSource = list;
   public void LoadNavItems() => lstNavItems.ItemsSource = Items.UI.GetNav();
   private void LoadNavItems(string filter, List<string> tags) =>
-    lstNavItems.ItemsSource = rbTagsAnd.IsChecked == true ?
-      Items.UI.GetNavFilterAnd(filter, tags) :
-      Items.UI.GetNavFilterOr(filter, tags);
+    lstNavItems.ItemsSource = rbTagsOr.IsChecked == true ?
+      Items.UI.GetNavFilterOr(filter, tags) :
+      Items.UI.GetNavFilterAnd(filter, tags);
 
   private void LstNavItems_SelectionChanged(object sender, SelectionChangedEventArgs e) => ReloadSelectedItem();
   private void LoadFilters() => tagFilter.ItemsSource = Data.UI.Tags.Get.AllTagsAndKeywords();
@@ -135,6 +136,40 @@ public partial class PP_Items : UserControl {
     var keyword = lstKeywords.SelectedItem.ToString();
     LoadKeywords(Keywords.SetImage(keyword, source));
   }
+
+  void OnChangeTags(Action ChangeTags) {
+    var currTags = Data.UI.Tags.Get.AllTagsAndKeywords();
+    ChangeTags();
+    if (!currTags.SequenceEqual(Data.UI.Tags.Get.AllTagsAndKeywords()))
+      LoadFilters();
+  }
+
+  private void OnCbTagsAdd(object sender, KeyEventArgs e) {
+    OnChangeTags(
+      () => {
+        if (e.Key != Key.Return)
+          return;
+
+        var tag = cbItemTags.Text.ToLower().Trim();
+        if (tag == "")
+          return;
+
+        cbItemTags.Text = "";
+
+        ForEachSelectedItem(uId => {
+          Items.AddTag(uId, tag);
+        });
+      });
+  }
+
+  private void OnDeleteTag(object sender, RoutedEventArgs e) {
+    OnChangeTags(() => {
+      var tag = ((ClickTagEventArgs)e).Tag;
+      ForEachSelectedItem(uId => {
+        Items.DelTag(uId, tag);
+      });
+    });
+  }
   #endregion
 
   #region Data importing
@@ -179,27 +214,5 @@ public partial class PP_Items : UserControl {
   }
 
   private void OnImportFromClipboard(object sender, RoutedEventArgs e) => ImportItems(Items.Import.FromClipboard);
-
-  private void OnCbTagsAdd(object sender, KeyEventArgs e) {
-    if (e.Key != Key.Return)
-      return;
-
-    var tag = cbItemTags.Text.ToLower().Trim();
-    if (tag == "")
-      return;
-
-    cbItemTags.Text = "";
-
-    ForEachSelectedItem(uId => {
-      Items.AddTag(uId, tag);
-    });
-  }
-
-  private void OnDeleteTag(object sender, RoutedEventArgs e) {
-    var tag = ((ClickTagEventArgs)e).Tag;
-    ForEachSelectedItem(uId => {
-      Items.DelTag(uId, tag);
-    });
-  }
   #endregion
 }

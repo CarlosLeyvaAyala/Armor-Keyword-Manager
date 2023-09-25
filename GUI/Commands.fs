c55a2047ead33ae33f1c17393fc71906ef30e921
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-09-24 22:44:17 -0600

    Clean up

--- GUI/Commands.fs
+++ GUI/Commands.fs
@@ -1,36 +1,7 @@
 ﻿namespace GUI.Commands
 
 open System.Windows.Input
-
-[<AutoOpen>]
-module private Internals =
-    type Cmd =
-        { name: string
-          text: string
-          keyDisplay: string
-          key: Key
-          modifiers: ModifierKeys }
-
-    let createCmd typeFunc v =
-        let inputs = InputGestureCollection()
-
-        inputs.Add(KeyGesture(v.key, v.modifiers, v.keyDisplay))
-        |> ignore
-
-        RoutedUICommand(v.text, v.name, typeFunc (), inputs)
-
-    let private blankCmd =
-        { name = ""
-          text = ""
-          keyDisplay = ""
-          key = Key.None
-          modifiers = ModifierKeys.None }
-
-    let basicCmd (create: Cmd -> RoutedUICommand) name text =
-        { blankCmd with
-            name = name
-            text = text }
-        |> create
+open DMLib_WPF.Commands
 
 [<Sealed>]
 type AppCmds private () =
